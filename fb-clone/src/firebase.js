8d19ba3b04dce88b12f4baabf4e04448e56cab2d
Isaac3N <theisaac3n@gmail.com>
2020-12-28 23:42:56 +0100

    facebook clone

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBTGOyp7i1TMzrNB-zhHs0zlkVllcArAQ4",
+    authDomain: "facebook-clone-50b96.firebaseapp.com",
+    projectId: "facebook-clone-50b96",
+    storageBucket: "facebook-clone-50b96.appspot.com",
+    messagingSenderId: "239365743168",
+    appId: "1:239365743168:web:b07294fe94419bd71a75a5",
+    measurementId: "G-GSZRD9BVKT"
+  };
